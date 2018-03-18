2a7838a222b9b95e22539a9ed1ddee55355b45de
caochong <18211159868@163.com>
2018-03-18 10:19:41 +0800

    开启本地服务

---
+++ SwiftLocalServer/HTTPServer/MyConnection/MyHTTPConnection.swift
@@ -0,0 +1,13 @@
+//
+//  MyHTTPConnection.swift
+//  SwiftLocalServer
+//
+//  Created by thundersoft on 2018/3/12.
+//  Copyright © 2018年 thundersoft. All rights reserved.
+//
+
+import UIKit
+
+class MyHTTPConnection: HTTPConnection {
+
+}
